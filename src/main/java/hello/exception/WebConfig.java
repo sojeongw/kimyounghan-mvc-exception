13679929ccb5faf1a687c964ae1e2da170a761fb
sojeongw <sojeongw.official@gmail.com>
2022-02-24 07:09:36 +0900

    API 예외 처리: HandlerExceptionResolver

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import resolver.MyHandlerExceptionResolver;
+import resolver.UserHandlerExceptionResolver;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
@@ -15,6 +16,13 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        // 만든 resolver를 등록해준다.
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -25,12 +33,6 @@ public class WebConfig implements WebMvcConfigurer {
                         "/css/**", "/*.ico"
                         , "/error", "/error-page/**" // 오류 페이지 경로
                 );
-    }
-
-    @Override
-    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
-        // 만든 resolver를 등록해준다.
-        resolvers.add(new MyHandlerExceptionResolver());
     }
 
     //    @Bean
