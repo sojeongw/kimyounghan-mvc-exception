ed0fc3431a8a95e941c6d12b7d47413fcbb6c559
sojeongw <sojeongw.official@gmail.com>
2022-02-28 06:53:16 +0900

    API 예외 처리: DefaultHandlerExceptionResolver

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -8,12 +8,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
 public class ApiExceptionController {
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
+    }
 
     @GetMapping("/api/response-status-ex1")
     public String responseStatusEx1() {
