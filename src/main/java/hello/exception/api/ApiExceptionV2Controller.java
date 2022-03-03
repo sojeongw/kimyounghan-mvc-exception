14aa581d1d70519ad04b29845f3f7f796162fa9e
sojeongw <sojeongw.official@gmail.com>
2022-03-04 06:49:34 +0900

    API 예외 처리: @ControllerAdvice

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,41 +1,16 @@
 package hello.exception.api;
 
 import hello.exception.exception.UserException;
-import hello.exception.exceptionhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
-
-    // 200 대신 다른 HTTP 상태 코드를 반환하고 싶을 때 사용한다.
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    // 이 컨트롤러 안에서 IllegalArgumentException이 터지면 이 메서드가 호출된다.
-    @ExceptionHandler(IllegalArgumentException.class)
-    // RestController이기 때문에 ErrorResult가 그대로 JSON으로 반환된다.
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-        log.error("[exceptionHandle] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandle(Exception e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
